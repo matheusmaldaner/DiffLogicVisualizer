be8b49687c769d32a6e0a66bff04988c4b96a057
Kristian O’Connor <kristianoconnor@ufl.edu>
2025-01-25 12:50:36 -0500

    Added requirements.txt, adjusted gitignore, removed hello api

--- mybackend/api/urls.py
+++ mybackend/api/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import HelloWorldAPIView, ModelAPIView
 
 urlpatterns = [
-    path('hello/', HelloWorldAPIView.as_view(), name='hello'),
     path('difflogic-models/', ModelAPIView.as_view(), name='model')
 ]
--- mybackend/api/views.py
+++ mybackend/api/views.py
@@ -1,10 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
-class HelloWorldAPIView(APIView):
-    def get(self, request):
-        return Response({"message": "Hello from Django!"}, status=status.HTTP_200_OK)
     
 class ModelAPIView(APIView):
     def get(self, request):
