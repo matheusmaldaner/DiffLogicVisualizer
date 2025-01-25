c7e4b1204be8d33739b6d32fc4a22472f51a8c76
Kristian O’Connor <kristianoconnor@ufl.edu>
2025-01-25 12:56:34 -0500

    Adjusted urls.py and views.py with different api structure

--- mybackend/api/urls.py
+++ mybackend/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HelloWorldAPIView, ModelAPIView
+from .views import ModelAPIView, ImageAPIView
+
 
 urlpatterns = [
-    path('difflogic-models/', ModelAPIView.as_view(), name='model')
+    path('difflogic-models/', ModelAPIView.as_view(), name='model'),
+    path('difflogic-images/', ImageAPIView.as_view(), name='image'),
 ]
--- mybackend/api/views.py
+++ mybackend/api/views.py
@@ -1,7 +1,21 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-    
+import os
+
+MODEL_DIR = "uploaded_models"
+IMAGE_DIR = "uploaded_images"
+
 class ModelAPIView(APIView):
-    def get(self, request):
-        return Response({"message": "Hello from Django!"}, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+       
+
+    def get(self, request, *args, **kwargs):
+        
+
+class ImageAPIView(APIView):
+
+    def post(self, request, *args, **kwargs):  
+
+    def get(self, request, *args, **kwargs):
